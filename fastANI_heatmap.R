387ec7693a6318476155a24c99adefaaef7f6f6e
spencer411 <43999021+spencer411@users.noreply.github.com>
2021-07-28 12:26:09 -0400

    Update fastANI_heatmap.R

--- fastANI_heatmap.R
+++ fastANI_heatmap.R
@@ -8,8 +8,8 @@ matrix <- acast(x, X~Y, value.var="ANI")
 
 ### define the colors within 2 zones
 breaks = seq(min(matrix), max(100), length.out=100)
-gradient1 = colorpanel( sum( breaks[-1]<=99.78 ), "blue", "white" )
-gradient2 = colorpanel( sum( breaks[-1]>99.78 & breaks[-1]<=100), "white", "red" )
+gradient1 = colorpanel( sum( breaks[-1]<=95), "blue", "white" )
+gradient2 = colorpanel( sum( breaks[-1]>95 & breaks[-1]<=100), "white", "red" )
 
 hm.colors = c(gradient1, gradient2)
 heatmap.2(matrix, scale = "none", trace = "none", col = hm.colors)
